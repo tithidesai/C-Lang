69b16c80e3080fce9ac5cf4e04957f4f4f2b71c9
Tithi <tithidesai71@gmail.com>
2023-06-21 09:11:40 +0530

    hello

--- pointer.c
+++ pointer.c
@@ -1,12 +1,7 @@
 #include<stdio.h>
 int main(){
-int a=20,*p,**q;
-p=&a;
-q=&p;
-printf("value of a is:%d\n",a);
-printf("address of a is:%u\n",&a);
-printf("address of using p is:%d\n",p);
-printf("address of p is ;%u\n",&p);
-printf("value of using p is :%d\n",*p);
-printf("value of a using q is :%d\n",**q);
-}
+  int a=20,*p,**q;
+  p=&a;
+  q=&p;
+  printf  
+} 
