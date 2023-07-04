1b66a0cba707bd3028f15d2b1238ffcabe39c664
Tithi <tithidesai71@gmail.com>
2023-07-04 09:53:37 +0530

    04/07/2023

---
+++ c++ programm/objectasarray.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+class test{
+         int n;
+         public:
+         void setn();
+         void getn(){
+               cout<<"value of n:"<<n<<endl;
+         }
+};
+void test::setn(){
+       cout<<"enter n:";
+       cin>>n;
+}
+int main()
+{
+         test a[5];
+         int i;
+         for(i=0;i<1;i++){
+                cout<<"no:"<<i+1<<endl;
+                a[i].setn();   
+         }
+         for(i=0;i<1;i++){
+                 cout<<"no:"<<i+1<<endl;
+                 a[i].getn();
+         }
+}   
